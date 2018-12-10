db4e36cca6c67a9d213f719397d84e59ed56e043
Freddie Wang <yunghsing.wang@linecorp.com>
2018-12-10 15:45:22 +0900

    Use AndroidX

---
+++ app/src/main/java/idv/freddiew/jetpackexamples/ui/main/MainFragment.kt
@@ -0,0 +1,31 @@
+package idv.freddiew.jetpackexamples.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import idv.freddiew.jetpackexamples.R
+
+class MainFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+}
