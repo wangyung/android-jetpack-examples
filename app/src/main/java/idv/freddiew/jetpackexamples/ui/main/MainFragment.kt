56084711254cbc46ce3e5fb6eb756a736a9b215b
Freddie Wang <yunghsing.wang@linecorp.com>
2018-12-10 17:16:37 +0900

    Add simple live data

--- app/src/main/java/idv/freddiew/jetpackexamples/ui/main/MainFragment.kt
+++ app/src/main/java/idv/freddiew/jetpackexamples/ui/main/MainFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import idv.freddiew.jetpackexamples.R
 import java.lang.IllegalStateException
@@ -12,19 +14,26 @@ import java.lang.IllegalStateException
 class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var simpleText: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
+    ): View =
+        inflater.inflate(R.layout.main_fragment, container, false).also {
+            simpleText = it.findViewById(R.id.simpleText)
+        }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = activity?.run {
             ViewModelProviders.of(this).get(MainViewModel::class.java)
         } ?: throw IllegalStateException("No view model")
+
+        viewModel.displayText.value = "In onActivityCreated"
+        viewModel.displayText.observe(this, Observer<String> { text ->
+            simpleText.text = text
+        })
     }
 
     companion object {
--- app/src/main/java/idv/freddiew/jetpackexamples/ui/main/MainViewModel.kt
+++ app/src/main/java/idv/freddiew/jetpackexamples/ui/main/MainViewModel.kt
@@ -1,7 +1,12 @@
 package idv.freddiew.jetpackexamples.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val displayText: MutableLiveData<String> = MutableLiveData()
+
+    init {
+        displayText.value = "Freddie's greeting"
+    }
 }
